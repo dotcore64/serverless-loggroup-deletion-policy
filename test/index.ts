60899a8983dec8ce161fd1740310a4ec877d6240
Julian Grinblat <julian@dotcore.co.il>
2024-10-09 23:37:44 +0900

    fix

--- test/index.ts
+++ test/index.ts
@@ -1,109 +1,81 @@
 import { join } from "node:path";
 import { createRequire } from "node:module";
 import { expect } from "chai";
-
-import logEmitter from "log/lib/emitter.js"; // eslint-disable-line n/no-missing-import
-import runServerless from "@serverless/test/run-serverless.js"; // eslint-disable-line n/no-missing-import
+import { execa } from "execa";
 
 const require = createRequire(import.meta.url);
-const serverlessRoot = join(require.resolve("serverless"), "..", "..");
 
-const logsBuffer = [];
-logEmitter.on("log", (event) => {
-  const {
-    logger: { namespace },
-    messageTokens,
-  } = event;
-  if (namespace === "serverless:plugin:serverless-loggroup-deletion-policy") {
-    logsBuffer.push(messageTokens[0]);
-  }
-});
+const isServerless3 = require("serverless/package.json").version.startsWith(
+  "3",
+);
+
+const runServerless = (cwd: string) =>
+  execa({ preferLocal: true, cwd, lines: true })`sls package --verbose`;
 
 describe("serverless-loggroup-deletion-policy", () => {
-  beforeEach(() => {
-    logsBuffer.splice(0, logsBuffer.length);
-  });
-
   describe("serverless plugin sets a DeletionPolicy correctly", () => {
     it("adds a `DeletionPolicy: Retain` to Log Group", async () => {
       const cwd = new URL("fixtures/retain", import.meta.url).pathname;
-      await runServerless(serverlessRoot, {
-        cwd,
-        command: "package",
-      });
+      const { stderr } = await runServerless(cwd);
 
       // eslint-disable-next-line import/no-dynamic-require
       const resources = require(
         join(cwd, ".serverless", "cloudformation-template-update-stack.json"),
       ).Resources;
 
-      expect(logsBuffer).to.deep.equal([
+      expect(stderr).to.include(
         "setting DeletionPolicy: Retain on /aws/lambda/retain-dev-hello",
-      ]);
+      );
       expect(resources.HelloLogGroup.DeletionPolicy).to.equal("Retain");
-    }).timeout(10_000); // the first call to `runServerless` is very expensive for some reason?
+    });
 
     it("adds a `DeletionPolicy: Delete` to Log Group", async () => {
       const cwd = new URL("fixtures/delete", import.meta.url).pathname;
-      await runServerless(serverlessRoot, {
-        cwd,
-        command: "package",
-      });
+      const { stderr } = await runServerless(cwd);
 
       // eslint-disable-next-line import/no-dynamic-require
       const resources = require(
         join(cwd, ".serverless", "cloudformation-template-update-stack.json"),
       ).Resources;
 
-      expect(logsBuffer).to.deep.equal([
+      expect(stderr).to.include(
         "setting DeletionPolicy: Delete on /aws/lambda/delete-dev-hello",
-      ]);
+      );
       expect(resources.HelloLogGroup.DeletionPolicy).to.equal("Delete");
     });
 
     it("adds a `DeletionPolicy: Retain` to Log Group via variable", async () => {
       const cwd = new URL("fixtures/variable", import.meta.url).pathname;
-      await runServerless(serverlessRoot, {
-        cwd,
-        command: "package",
-      });
+      const { stderr } = await runServerless(cwd);
 
       // eslint-disable-next-line import/no-dynamic-require
       const resources = require(
         join(cwd, ".serverless", "cloudformation-template-update-stack.json"),
       ).Resources;
 
-      expect(logsBuffer).to.deep.equal([
+      expect(stderr).to.include(
         "setting DeletionPolicy: Retain on /aws/lambda/variable-dev-hello",
-      ]);
+      );
       expect(resources.HelloLogGroup.DeletionPolicy).to.equal("Retain");
     });
   });
 
   describe("plugin configuration and usage", () => {
-    it("errors out if using non-aws provider", async () => {
-      const cwd = new URL("fixtures/gcp", import.meta.url).pathname;
+    if (isServerless3) {
+      it("errors out if using non-aws provider", async () => {
+        const cwd = new URL("fixtures/gcp", import.meta.url).pathname;
 
-      return expect(
-        runServerless(serverlessRoot, {
-          cwd,
-          command: "package",
-        }),
-      ).to.be.rejectedWith(
-        Error,
-        "serverless-loggroup-deletion-policy can only be used with the 'aws' provider",
-      );
-    });
+        return expect(runServerless(cwd)).to.be.rejectedWith(
+          Error,
+          "serverless-loggroup-deletion-policy can only be used with the 'aws' provider",
+        );
+      });
+    }
 
     it("errors out on missing logGroupDeletionPolicy config object", async () => {
       const cwd = new URL("fixtures/invalid", import.meta.url).pathname;
 
-      return expect(
-        runServerless(serverlessRoot, {
-          cwd,
-          command: "package",
-        }),
-      ).to.be.rejectedWith(
+      return expect(runServerless(cwd)).to.be.rejectedWith(
         Error,
         "Please specify `logGroupDeletionPolicy` in your serverless.yml file!",
       );
@@ -112,12 +84,7 @@ describe("serverless-loggroup-deletion-policy", () => {
     it("errors out on missing keepLambdaLogs", async () => {
       const cwd = new URL("fixtures/non-boolean", import.meta.url).pathname;
 
-      return expect(
-        runServerless(serverlessRoot, {
-          cwd,
-          command: "package",
-        }),
-      ).to.be.rejectedWith(
+      return expect(runServerless(cwd)).to.be.rejectedWith(
         Error,
         /Configuration error at 'custom.logGroupDeletionPolicy.keepLambdaLogs': must be boolean/,
       );
@@ -127,12 +94,7 @@ describe("serverless-loggroup-deletion-policy", () => {
       const cwd = new URL("fixtures/non-boolean-warn", import.meta.url)
         .pathname;
 
-      return expect(
-        runServerless(serverlessRoot, {
-          cwd,
-          command: "package",
-        }),
-      ).to.be.rejectedWith(
+      return expect(runServerless(cwd)).to.be.rejectedWith(
         Error,
         "Please specify `logGroupDeletionPolicy.keepLambdaLogs` with true or false in your serverless.yml file!",
       );
